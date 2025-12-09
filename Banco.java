9ba8b126f58d5fc0bd08c3592b4ab35c08586183
cfo-cpu <cfo@discente.ifpe.edu.br>
2025-12-09 20:55:29 -0300

    Create Banco.java

---
+++ Banco.java
@@ -0,0 +1,18 @@
+package Entidade;
+
+public class Banco{
+
+    private String agencia;
+    private String numeroConta;
+
+    public Banco( String agencia, String numeroConta){
+            this.agencia = agencia;
+            this.numeroConta = numeroConta;
+    }
+   public String getAgencia(){
+      return agencia;
+   }
+  public String getNmueroConta(){
+      return NumeroConta;
+   }
+  }
