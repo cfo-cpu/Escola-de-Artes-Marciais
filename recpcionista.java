94e29b099beab127654f00ead6ec74757072e7f4
cfo-cpu <cfo@discente.ifpe.edu.br>
2025-12-09 21:17:59 -0300

    Update recpcionista.java

--- recpcionista.java
+++ recpcionista.java
@@ -1,26 +1,26 @@
 import java.util.List;
 
 public class recpcionista extends funcionarios{
-    protected float id_recepcionista;
+    protected float id;
     protected String turno;
 
     public recpcionista(String matricula, String CPF, String nome, String dataNascimento,
 			String agencia, String numeroConta, int experiencia, String email, 
-			List<String> telefones, float id_recepcionista, String turno) {
+			List<String> telefones, float i, String turno) {
             super(matricula, CPF, nome, dataNascimento, agencia, numeroConta, experiencia, email, telefones);
 
-            this.id_recepcionista=id_recepcionista;
+            this.id=id;
             this.turno=turno;	
         
     }
-    public Float getId_recepcionista(){
-        return id_recepcionista;
+    public Float getId(){
+        return id;
     }
     public String getTurno(){
         return turno;
     }
-    public void setId_recepcionista(Float id_recepcionista){
-        this.id_recepcionista=id_recepcionista;
+    public void setId(Float id){
+        this.id=id;
     }
     public void setTurno(String turno){
         this.turno=turno;
